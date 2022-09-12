41871898577f282a2ce858bb4de5119a137f4081
louis mulinge <louismulinge@gmail.com>
2022-09-12 22:06:43 +0100

    hero section btn

--- src/components/HeroSection/HeroElements.js
+++ src/components/HeroSection/HeroElements.js
@@ -86,6 +86,7 @@ export const HeroBtnWrapper = styled.div`
   display: flex;
   flex-direction: column;
   align-items: center;
+  color: #fff
 `;
 
 export const ArrowForward = styled(MdArrowForward)`
